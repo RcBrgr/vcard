acbb40e5540c9bf5caf153f9dc170a726603a13c
Eric Berger <bergerberic@googlemail.com>
2025-09-16 22:09:29 +0200

    lint: formatted VCardVersion

--- src/main/kotlin/vcard/model/VCardVersion.kt
+++ src/main/kotlin/vcard/model/VCardVersion.kt
@@ -3,7 +3,9 @@ package com.github.rcbrgr.vcard.model
 /**
  * Defines the vCard version.
  */
-enum class VCardVersion(val version: String) {
+enum class VCardVersion(
+    val version: String,
+) {
     V2_1("2.1"),
     V3_0("3.0"),
     V4_0("4.0"),
